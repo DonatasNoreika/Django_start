3a7ba9e95033df0c3d5665316a56c61e05b781f7
Donoras <donoras@gmail.com>
2021-05-10 20:50:21 +0300

    7 email_settings.py

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -25,3 +25,4 @@ urlpatterns = [
     path('', RedirectView.as_view(url='library/', permanent=True)),
 ] + (static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) +
     static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
+
